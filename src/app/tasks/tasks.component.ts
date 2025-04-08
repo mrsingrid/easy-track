03037bb31e622ffd9127c5964ffea2c34dc84ff1
Ingrid Araújo <ara.ingridce@gmail.com>
2025-04-08 11:14:24 -0300

    Implement component dialog new task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -49,5 +51,22 @@ export class TasksComponent {
 
   onStartAddTask() {
     this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+
+    this.isAddingTask = false;
   }
 }
