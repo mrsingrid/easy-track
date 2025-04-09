45f8c42b7aef7cebecc1949fe01633ddc6ec1c42
Ingrid Araújo <ara.ingridce@gmail.com>
2025-04-09 12:46:44 -0300

    Migrates app to use modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, SharedModule, TasksModule],
+})
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// Utilizando componentes autonomos
 
-import { AppComponent } from './app/app.component';
+// import { bootstrapApplication } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
